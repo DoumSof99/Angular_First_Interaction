74c349c85e5f2b4811eeafefd07f1722ef3a6e22
DoumSof99 <71881384+DoumSof99@users.noreply.github.com>
2023-03-30 00:50:30 +0300

    Added names and descriptions of products using HTML tags with Directives and Event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { productsDataObject } from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = products;
+  doProducts = productsDataObject;
 
   share() {
     window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,31 +1,30 @@
-export interface Product {
+export interface IProduct {
   id: number;
   name: string;
   price: number;
   description: string;
 }
 
-export const products = [
+export const productsDataObject = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'IPhone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'IPhone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Android Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
